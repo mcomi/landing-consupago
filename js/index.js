a5e2f8f3e5f66c4585266a34dbe0be2fad292438
Manuel Comi <mcomi@hotmail.com>
2018-05-29 20:28:34 -0500

    cambios estilos e imagenes

--- js/index.js
+++ js/index.js
@@ -7,6 +7,25 @@ $.fn.exists = function() {
   return this.length > 0;
 }
 
+if ($('#celular').exists()) {
+
+  let cleave = new Cleave('#celular', {
+    phone: true,
+    phoneRegionCode: 'MX'
+  });
+
+  let celCheck = new Cleave('#celular-check', {
+    phone: true,
+    phoneRegionCode: 'MX'
+  })
+}
+
+var formatter = new Intl.NumberFormat('es-MX', {
+  style: 'currency',
+  currency: 'MXN',
+  minimumFractionDigits: 2,
+});
+
 $('.slider-nav').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
@@ -28,13 +47,41 @@ $('.slider-nav').on('afterChange', function(event, slick, currentSlide, nextSlid
 
 function calculaMensualidades(val){
     var monto = parseInt(val);
-    monto12Meses.html(Math.round((monto / 12), 2));
-    monto24Meses.html(Math.round((monto / 24), 2));
-    monto36Meses.html(Math.round((monto / 36), 2));
-    monto48Meses.html(Math.round((monto / 48), 2));
+    monto12Meses.html(formatter.format((monto / 12).toFixed(2)));
+    monto24Meses.html(formatter.format((monto / 24).toFixed(2)));
+    monto36Meses.html(formatter.format((monto / 36).toFixed(2)));
+    monto48Meses.html(formatter.format((monto / 48).toFixed(2)));
 }
 
 $(document).scroll(function () {
   var $nav = $(".navbar-fixed-top");
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
+
+
+
+const boxesPagoPorMes = $('.pago-por-mes')
+boxesPagoPorMes.each(function() {
+    let box = $(this);
+    box.click(function(){
+      boxesPagoPorMes.each(function(){
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+      var valorMeses = $(this).find('span').attr('id');  // obtengo el valor de meses elegidos
+    })
+  });
+
+  $(".step-img").hover(function(){
+    $(this).addClass('animated pulse');
+});
+$(".step-img").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
+    $(this).removeClass('animated pulse');
+});
+
+$(".icon-item").hover(function(){
+  $(this).addClass('animated pulse');
+});
+$(".icon-item").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
+  $(this).removeClass('animated pulse');
 });
